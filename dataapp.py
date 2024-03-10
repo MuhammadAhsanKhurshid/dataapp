e9e27e237259e80454a43afa792fac7471a8601c
Muhammad Ahsan Khurshid <149896829+MuhammadAhsanKhurshid@users.noreply.github.com>
2024-03-10 15:06:07 +0500

    Update dataapp.py

--- dataapp.py
+++ dataapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
+
 st.write("This app is purely dedicated to data analyticsss")
 
 sales = pd.read_csv(r"Sales.csv")
@@ -9,5 +9,4 @@ sales
 s1 = sales.groupby('Export Class').agg(Total_Sales=("Revenue","sum"),Total_Quantity=("Quantity Sold","sum"))
 s1
 
-s2 = sns.jointplot(x = 'Revenue', y = 'Quantity Sold',data = sales)
-s2
+
