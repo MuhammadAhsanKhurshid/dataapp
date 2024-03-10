4daa987f6eba8da0f9e6f261b2bb094a6d3f662c
Muhammad Ahsan Khurshid <149896829+MuhammadAhsanKhurshid@users.noreply.github.com>
2024-03-10 14:37:23 +0500

    dataapp.py

--- dataapp.py
+++ dataapp.py
@@ -6,4 +6,5 @@ st.write("This app is purely dedicated to data analyticsss")
 sales = pd.read_csv(r"Sales.csv")
 sales
 
-
+s1 = sales.groupby('Export Class').agg(Total_Sales=("Revenue","sum"),Total_Quantity=("Quantity Sold","sum"))
+s1
