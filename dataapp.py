eb812105e445e062096140316ce1feabd5a70cec
Muhammad Ahsan Khurshid <149896829+MuhammadAhsanKhurshid@users.noreply.github.com>
2024-03-10 14:09:56 +0500

    Add files via upload

---
+++ dataapp.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.write("This app is purely dedicated to data analyticsss")
+
+sales = pd.read_csv(r"sales.csv")
+sales
+s1 = sales.groupby('Export Class').agg(Total_Sales=("Revenue","sum"),Total_Quantity=("Quantity Sold","sum"))
+s1
+
