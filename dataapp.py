fda488aa2ab8d1bd9ba07f40b722164532bb5a02
Muhammad Ahsan Khurshid <149896829+MuhammadAhsanKhurshid@users.noreply.github.com>
2024-03-17 16:30:58 +0500

    Update dataapp.py

--- dataapp.py
+++ dataapp.py
@@ -12,6 +12,11 @@ time_hist_color = st.sidebar.selectbox('Color by', ('temp_min','temp_max'))
 st.sidebar.subheader('Donut chart parameter')
 donut_theta = st.sidebar.selectbox('Select data',('q2', 'q3'))
 
+st.sidebar.subheader('Line chart parameters')
+plot_data = st.sidebar.multiselect('Select data',['Price','Quantity'],['Price','Quantity'])
+plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
+
+
 
 st.sidebar.markdown('''
 ---
