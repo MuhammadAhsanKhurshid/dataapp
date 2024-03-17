b531c374dcf2ec7b1732a0ba221d35b1a403704b
Muhammad Ahsan Khurshid <149896829+MuhammadAhsanKhurshid@users.noreply.github.com>
2024-03-17 12:17:09 +0500

    dataapp.py

--- dataapp.py
+++ dataapp.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 st.write("This app is purely dedicated to data analyticsss")
 st.title("Homepage")
+iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
+separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
 sales = pd.read_csv(r"Sales.csv")
 sales
 
